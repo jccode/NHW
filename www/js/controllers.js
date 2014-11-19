7336ca7ad41ad5142966df62398cde0757f1ccd5
jcchen <junchangchen@gmail.com>
2014-11-19 16:11:24 +0800

    disallow user change seat if user outside building

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -368,12 +368,12 @@ angular.module("nhw.controllers", ['nhw.services'])
 
                 var t = (el.classed('seat-cuser') || el.classed('seat-unavailable'))
                         ? "user"
-                        : (hascheckin ? "seat_change" : "workspace");
+                        : (hascheckin && $rootScope.isInBuilding ? "seat_change" : "workspace");
 
                 // disallow user to change seat, if user not in building
-                if(!$rootScope.isInBuilding && t == "seat_change")  {
-                    return;
-                }
+                // if(!$rootScope.isInBuilding && t == "seat_change")  {
+                //     return;
+                // }
                 
                 $scope.$apply(function() {
                     $scope.popup = t;
