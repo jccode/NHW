66545683cf1e96af5c469296c7963a2e982d7883
jcchen <junchangchen@gmail.com>
2015-01-11 12:53:39 +0800

    ios8 notification

--- runplugins.sh
+++ runplugins.sh
@@ -7,8 +7,9 @@
 cordova plugin add org.apache.cordova.device
 cordova plugin add org.apache.cordova.file
 cordova plugin add org.apache.cordova.file-transfer
-# cordova plugin add https://github.com/petermetz/cordova-plugin-ibeacon.git
-cordova plugin add https://github.com/jccode/cordova-plugin-ibeacon.git
+cordova plugin add org.apache.cordova.console
+cordova plugin add https://github.com/petermetz/cordova-plugin-ibeacon.git
+# cordova plugin add https://github.com/jccode/cordova-plugin-ibeacon.git
 cordova plugin add de.appplant.cordova.plugin.local-notification
 cordova plugin add https://github.com/randdusing/BluetoothLE
 cordova plugin add https://github.com/wildabeast/BarcodeScanner
