3533bcfe154214678593a0cf13413c520e326ad5
jcchen <junchangchen@gmail.com>
2015-04-19 23:55:49 +0800

    add angular-gettext

---
+++ www/Gruntfile.js
@@ -0,0 +1,32 @@
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // uglify: {
+        //     options: {
+        //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        //     },
+        //     build: {
+        //         src: 'src/<%= pkg.name %>.js',
+        //         dest: 'build/<%= pkg.name %>.min.js'
+        //     }
+        // },
+        nggettext_extract: {
+            pot: {
+                files: {
+                    'po/template.pot': ['partials/*.html']
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-angular-gettext');
+
+    // Default task(s).
+    grunt.registerTask('default', ['nggettext_extract']);
+
+};
